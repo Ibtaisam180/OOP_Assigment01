1106a5f53ed5e0c242495441d2a044dad764efbd
Ibtaisam180 <ibtaisam180khan16@gmail.com>
2025-10-22 18:53:20 +0500

    Update student.h

--- oop/student.h
+++ oop/student.h
@@ -1,13 +1,10 @@
 #pragma once
-// Student.h
-#ifndef STUDENT_H       // Header guard
-#define STUDENT_H
 
 #include<iostream>
 using namespace std;
 class student {
 private:
-// Encapsulation: private data members cannot be accessed directly
+// Encapsulation
 	string studentname;
 	int studentage;
 	int rollno;
@@ -17,23 +14,19 @@ public:
 	student(string n, int a, int r, float g);
 
 	~student();
-// Setter methods (Encapsulation)
+// Setter methods
 	void setname(string n);
 	void setage(int a);
 	void setrollno(int r);
 	void setgpa(float g);
-	// Getter methods (Encapsulation)
+	// Getter methods 
 	string getname();
 	int getage();
 	int getrollno();
 	float getgpa();
- // Abstraction: only showing relevant functionality
+ // Abstraction
 	void displayinfo();
 	void calculategrade();
 	    
 
-
-
-
 };
-#endif //end of header guard
